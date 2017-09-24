5bea72095d8d2ce4bb9345f3cd1bc0d45061f3c2
Sean Lobo <seanlobo2010@gmail.com>
2017-09-24 11:41:14 -0700

    Implement initial Tag and Item classes

---
+++ python/ground/item.py
@@ -0,0 +1,17 @@
+import tag.Tag as Tag
+
+
+class Item:
+
+    def __init__(self, json_payload):
+        self._id   = json_payload['id']
+        self._tags = json_payload['tags'] or {}
+
+        for key, value in list(self._tags.items()):
+            self._tag[key] = Tag(value)
+
+    def get_id(self):
+        return self._id
+
+    def get_tags(self):
+        return self._tags
