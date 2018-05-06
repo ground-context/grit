65b4d1e77eea5d4fe1ce76cc5f4dce8be583ca56
Nitin Manivasagan <nitinmani@berkeley.edu>
2018-05-05 18:25:49 -0700

    Added edge version tests, as well as timers and memory usage checks

--- python/tests/test_grit.py
+++ python/tests/test_grit.py
@@ -1,8 +1,11 @@
 import unittest
 import uuid
+import time
 
 import ground.grit as grit
 import ground.common.model as model
+
+from memory_profiler import profile
 
 class TestClient(unittest.TestCase):
 
@@ -10,8 +13,10 @@ class TestClient(unittest.TestCase):
     def setUpClass(cls):
         cls.client = grit.GroundClient('git')
 
+    @profile
     def test_node_create(self):
         source_key = uuid.uuid4().hex
+        start = time.time()
         node = self.client.createNode(source_key, source_key)
 
         self.assertTrue(
@@ -34,14 +39,18 @@ class TestClient(unittest.TestCase):
         with self.assertRaises(FileExistsError):
             self.client.createNode(source_key, source_key)
 
+        elapsed = time.time() - start
+        print('Time elapsed is: ' + elapsed + " seconds")
         return node
 
+    @profile
     def test_node_get(self):
         node = self.test_node_create()
 
         # I can try to get node that exists, or node that not exists
 
         # Get node that exists
+        start = time.time()
         got_node = self.client.getNode(node.get_source_key())
         self.assertTrue(
             got_node is not None,
@@ -60,7 +69,10 @@ class TestClient(unittest.TestCase):
         # Get node that not exists
         with self.assertRaises(FileNotFoundError):
             self.client.getNode(uuid.uuid4().hex)
+        elapsed = time.time() - start
+        print('Time elapsed is: ' + elapsed + " seconds")
 
+    @profile
     def test_node_version_create(self):
         node = self.test_node_create()
 
@@ -68,6 +80,7 @@ class TestClient(unittest.TestCase):
         #   or nv for node that not exists
 
         # Create a node_version for a node that exists
+        start = time.time()
         node_version = self.client.createNodeVersion(int(node.get_id()))
         self.assertTrue(
             node_version is not None,
@@ -112,8 +125,11 @@ class TestClient(unittest.TestCase):
             self.client.createNodeVersion(str(uuid.uuid4().int))
 
         # Total: created two distinct node versions for the same node.
+        elapsed = time.time() - start
+        print('Time elapsed is: ' + elapsed + " seconds")
         return node_version
 
+    @profile
     def test_node_version_get_chain(self):
         node = self.test_node_create()
 
@@ -121,6 +137,8 @@ class TestClient(unittest.TestCase):
         nv2 = self.client.createNodeVersion(node.get_id(), parentIds=[nv1.get_id()])
         nv3 = self.client.createNodeVersion(node.get_id(), parentIds=[nv2.get_id()])
 
+        start = time.time()
+
         node_version = self.client.getNodeVersion(nv2.get_id())
 
         self.assertTrue(
@@ -141,9 +159,12 @@ class TestClient(unittest.TestCase):
             node_version == nv2,
             msg="Stored and retrieved node versions mismatch"
         )
+        elapsed = time.time() - start
+        print('Time elapsed is: ' + elapsed + " seconds")
 
         return node, nv1, nv2, nv3
 
+    @profile
     def test_node_version_get_fan(self):
         node = self.test_node_create()
 
@@ -151,6 +172,8 @@ class TestClient(unittest.TestCase):
         nv2 = self.client.createNodeVersion(node.get_id())
         nv3 = self.client.createNodeVersion(node.get_id())
 
+        start = time.time()
+
         node_version = self.client.getNodeVersion(nv2.get_id())
 
         self.assertTrue(
@@ -171,9 +194,12 @@ class TestClient(unittest.TestCase):
             node_version == nv2,
             msg="Stored and retrieved node versions mismatch"
         )
+        elapsed = time.time() - start
+        print('Time elapsed is: ' + elapsed + " seconds")
 
         return node, nv1, nv2, nv3
 
+    @profile    
     def test_node_version_get_fan_out_in(self):
         node = self.test_node_create()
 
@@ -182,6 +208,7 @@ class TestClient(unittest.TestCase):
         nv3 = self.client.createNodeVersion(node.get_id())
 
         nv4 = self.client.createNodeVersion(node.get_id(), parentIds=[nv1.get_id(), nv2.get_id(), nv3.get_id()])
+        start = time.time()
 
         node_version = self.client.getNodeVersion(nv2.get_id())
 
@@ -224,8 +251,11 @@ class TestClient(unittest.TestCase):
             node_version == nv4,
             msg="Stored and retrieved node versions mismatch"
         )
+        elapsed = time.time() - start
+        print('Time elapsed is: ' + elapsed + " seconds")
 
 
+    @profile
     def test_node_version_get_dag(self):
         node = self.test_node_create()
 
@@ -236,6 +266,8 @@ class TestClient(unittest.TestCase):
         nv4 = self.client.createNodeVersion(node.get_id(), parentIds=[nv2.get_id(), nv3.get_id()])
         nv5 = self.client.createNodeVersion(node.get_id(), parentIds=[nv4.get_id()])
         nv6 = self.client.createNodeVersion(node.get_id(), parentIds=[nv4.get_id()])
+
+        start = time.time()
 
         node_version = self.client.getNodeVersion(nv2.get_id())
 
@@ -341,8 +373,11 @@ class TestClient(unittest.TestCase):
             node_version == nv6,
             msg="Stored and retrieved node versions mismatch"
         )
+        elapsed = time.time() - start
+        print('Time elapsed is: ' + elapsed + " seconds")
 
 
+    @profile
     def test_node_version_latest_get(self):
         node = self.test_node_create()
 
@@ -354,6 +389,7 @@ class TestClient(unittest.TestCase):
         nv5 = self.client.createNodeVersion(node.get_id(), parentIds=[nv4.get_id()])
         nv6 = self.client.createNodeVersion(node.get_id(), parentIds=[nv4.get_id()])
 
+        start = time.time()
         node_versions = self.client.getNodeLatestVersions(node.get_source_key())
 
         for node_version in node_versions:
@@ -384,7 +420,10 @@ class TestClient(unittest.TestCase):
             node_versions[0] != node_versions[1],
             msg='getNodeLatestVersions returned two node versions that are equal but should be different'
         )
+        elapsed = time.time() - start
+        print('Time elapsed is: ' + elapsed + " seconds")
 
+    @profile
     def test_node_get_history(self):
         # dag
         node = self.test_node_create()
@@ -397,6 +436,7 @@ class TestClient(unittest.TestCase):
         nv5 = self.client.createNodeVersion(node.get_id(), parentIds=[nv4.get_id()])
         nv6 = self.client.createNodeVersion(node.get_id(), parentIds=[nv4.get_id()])
 
+        start = time.time()
         dag = self.client.getNodeHistory(node.get_source_key())
 
         self.assertTrue(
@@ -428,6 +468,9 @@ class TestClient(unittest.TestCase):
             msg="Expected 6 elements but found {}".format(len(dag))
         )
 
+        elapsed = time.time() - start
+        print('Time elapsed in dag is: ' + elapsed + " seconds")
+
         # fan out in
         node = self.test_node_create()
 
@@ -436,6 +479,8 @@ class TestClient(unittest.TestCase):
         nv3 = self.client.createNodeVersion(node.get_id())
 
         nv4 = self.client.createNodeVersion(node.get_id(), parentIds=[nv1.get_id(), nv2.get_id(), nv3.get_id()])
+
+        start = time.time()
 
         dag = self.client.getNodeHistory(node.get_source_key())
 
@@ -459,6 +504,8 @@ class TestClient(unittest.TestCase):
             len(dag) == 4,
             msg="Expected 4 elements but found {}".format(len(dag))
         )
+        elapsed = time.time() - start
+        print('Time elapsed in fan out in is: ' + elapsed + " seconds")
 
 
         # chain
@@ -467,6 +514,8 @@ class TestClient(unittest.TestCase):
         nv1 = self.client.createNodeVersion(node.get_id())
         nv2 = self.client.createNodeVersion(node.get_id(), parentIds=[nv1.get_id()])
         nv3 = self.client.createNodeVersion(node.get_id(), parentIds=[nv2.get_id()])
+
+        start = time.time()
 
         dag = self.client.getNodeHistory(node.get_source_key())
 
@@ -486,7 +535,10 @@ class TestClient(unittest.TestCase):
             len(dag) == 3,
             msg="Expected 3 elements but found {}".format(len(dag))
         )
+        elapsed = time.time() - start
+        print('Time elapsed in chain is: ' + elapsed + " seconds")
 
+    @profile
     def test_edge_create(self):
         # There are two alternatives:
         # Create an edge between two existing nodes
@@ -497,6 +549,7 @@ class TestClient(unittest.TestCase):
         node2 = self.test_node_create()
 
         source_key = uuid.uuid4().hex
+        start = time.time()
         edge = self.client.createEdge(source_key, node1.get_id(), node2.get_id())
 
         self.assertTrue(
@@ -523,11 +576,16 @@ class TestClient(unittest.TestCase):
         with self.assertRaises(FileExistsError):
             self.client.createEdge(source_key, node2.get_id(), node1.get_id())
 
+        elapsed = time.time() - start
+        print('Time elapsed is: ' + elapsed + " seconds")
+
         return edge
 
 
+    @profile
     def test_edge_get(self):
         edge = self.test_edge_create()
+        start = time.time()
 
         # I can try to get edge that exists or edge that not exists
 
@@ -551,6 +609,10 @@ class TestClient(unittest.TestCase):
         with self.assertRaises(FileNotFoundError):
             self.client.getEdge(uuid.uuid4().hex)
 
+        elapsed = time.time() - start
+        print('Time elapsed is: ' + elapsed + " seconds")
+
+    @profile
     def test_edge_version_create(self):
         edge = self.test_edge_create()
         from_node_id = edge.get_from_node_id()
@@ -562,6 +624,7 @@ class TestClient(unittest.TestCase):
         # Create an edge_version for an edge that exists
         from_node_version = self.client.createNodeVersion(from_node_id)
         to_node_version = self.client.createNodeVersion(to_node_id)
+        start = time.time()
         edge_version = self.client.createEdgeVersion(edge.get_id(), to_node_version.get_id(),
                                                      from_node_version.get_id())
         self.assertTrue(
@@ -584,16 +647,96 @@ class TestClient(unittest.TestCase):
             self.client.createNodeVersion(uuid.uuid4().int, to_node_version.get_id(),
                                           from_node_version.get_id())
 
+        elapsed = time.time() - start
+        print('Time elapsed is: ' + elapsed + " seconds")
+
         return edge_version
 
+    @profile
     def test_edge_version_get(self):
-        pass
+        edge = self.test_edge_create()
+        from_node_id = edge.get_from_node_id()
+        to_node_id = edge.get_to_node_id()
 
+        # Create an edge version for an edge that exists
+        from_node_version = self.client.createNodeVersion(from_node_id)
+        to_node_version = self.client.createNodeVersion(to_node_id)
+        edge_version = self.client.createEdgeVersion(edge.get_id(), to_node_version.get_id(),
+                                                     from_node_version.get_id())
+
+        start = time.time()
+        got_edge_version = self.client.getEdgeVersion(edge.get_id())
+        self.assertTrue(
+            got_edge_version is not None,
+            msg='valid call to getEdgeVersion returned None'
+        )
+        self.assertTrue(
+            type(got_edge_version) == model.core.edge.EdgeVersion,
+            msg="getEdgeVersion returned edge version that is of type '{}' rather than 'EdgeVersion'"
+            .format(type(edge))
+        )
+        self.assertTrue(
+            got_edge_version == edge_version,
+            msg='valid call to getEdgeVersion returned incorrect data'
+        )
+
+        elapsed = time.time() - start
+        print('Time elapsed is: ' + elapsed + " seconds")
+
+    @profile
     def test_edge_version_latest_get(self):
-        pass
+        n1 = self.test_node_create()
+        n2 = self.test_node_create()
 
+        sk1 = uuid.uuid4().hex
+        sk2 = uuid.uuid4().hex
+
+        edge = self.client.createEdge(sk1, n1.get_id(), n2.get_id())
+        ev1 = self.client.createEdgeVersion(edge.get_id(), n1.get_id(), n2.get_id())
+        ev2 = self.client.createEdgeVersion(e1.get_id(), n1.get_id(), n2.get_id(), parentIds=ev1.get_id())
+        ev3 = self.client.createEdgeVersion(e1.get_id(), n1.get_id(), n2.get_id(), parentIds=ev2.get_id())
+        ev4 = self.client.createEdgeVersion(e1.get_id(), n1.get_id(), n2.get_id(), parentIds=ev3.get_id())
+        ev5 = self.client.createEdgeVersion(e1.get_id(), n1.get_id(), n2.get_id(), parentIds=ev3.get_id())
+
+        start = time.time()
+        edge_versions = self.client.getEdgeLatestVersions(node.get_source_key())
+
+        for edge_version in edge_versions:
+            self.assertTrue(
+                edge_version is not None,
+                msg='getEdgeLatestVersions with edge_id={} returned None instead of a edge version'
+                .format(node.get_id())
+            )
+            self.assertTrue(
+                type(edge_version) == model.core.node_version.EdgeVersion,
+                msg="getEdgeLatestVersions returned edgeVersion of type '{}' rather than 'EdgeVersion'"
+                .format(type(edge_version))
+            )
+            self.assertTrue(
+                edge_version.get_edge_id() == edge.get_id(),
+                msg="getEdgeLatestVersions edge_id does not match id of edge"
+            )
+            self.assertTrue(
+                edge_version == ev4 or edge_version == ev5,
+                msg="Stored and retrieved edge versions mismatch"
+            )
+        self.assertTrue(
+            len(edge_versions) == 2,
+            msg='getEdgeLatestVersions returned {} edge versions but there are 2 latest.'
+                .format(len(edge_versions))
+        )
+        self.assertTrue(
+            edge_versions[0] != edge_versions[1],
+            msg='getEdgeLatestVersions returned two edge versions that are equal but should be different'
+        )
+        
+        elapsed = time.time() - start
+        print('Time elapsed is: ' + elapsed + " seconds")
+
+    @profile
     def test_lineage_edge_create(self):
         source_key = uuid.uuid4().hex
+        start = time.time()
         node = self.client.createLineageEdge(source_key, source_key)
 
         self.assertTrue(
@@ -616,14 +759,19 @@ class TestClient(unittest.TestCase):
         with self.assertRaises(FileExistsError):
             self.client.createLineageEdge(source_key, source_key)
 
+        elapsed = time.time() - start
+        print('Time elapsed is: ' + elapsed + " seconds")
+
         return node
 
+    @profile
     def test_lineage_edge_get(self):
         node = self.test_lineage_edge_create()
 
         # I can try to get node that exists, or node that not exists
 
         # Get node that exists
+        start = time.time()
         got_node = self.client.getLineageEdge(node.get_source_key())
         self.assertTrue(
             got_node is not None,
@@ -643,6 +791,10 @@ class TestClient(unittest.TestCase):
         with self.assertRaises(FileNotFoundError):
             self.client.getLineageEdge(uuid.uuid4().hex)
 
+        elapsed = time.time() - start
+        print('Time elapsed is: ' + elapsed + " seconds")
+
+    @profile
     def test_lineage_edge_version_create(self):
         lineage_edge = self.test_lineage_edge_create()
         nv1 = self.test_node_version_create()
@@ -652,6 +804,7 @@ class TestClient(unittest.TestCase):
         #   or ev for an edge that not exists
 
         # Create an edge_version for an edge that exists
+        start = time.time()
         edge_version = self.client.createLineageEdgeVersion(lineage_edge.get_id(), nv2.get_id(), nv1.get_id())
         self.assertTrue(
             edge_version is not None,
@@ -672,6 +825,9 @@ class TestClient(unittest.TestCase):
         with self.assertRaises(KeyError):
             self.client.createNodeVersion(uuid.uuid4().int, nv2.get_id(),
                                           nv1.get_id())
+
+        elapsed = time.time() - start
+        print('Time elapsed is: ' + elapsed + " seconds")
 
         return edge_version
 
